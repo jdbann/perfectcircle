1180a6dc704fbe322d67adf3e51d4ecd02cdf274
John Bannister <jdbann@gmail.com>
2016-09-18 17:43:18 +0100

    Added login test and news posting test
    Closes #6 (or at least starts to)

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "user_can_login", type: :feature do
+  scenario "user logs in" do
+    user = create :user, email: "john@example.com", password: "password"
+
+    visit new_user_session_path
+
+    fill_in :user_email, with: "john@example.com"
+    fill_in :user_password, with: "password"
+    click_on "Sign in"
+
+    expect(page).to have_text("Signed in successfully.")
+  end
+end
---
+++ spec/features/user_creates_news_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "user_creates_news", type: :feature do
+  scenario "with title and body" do
+    login_as(create :user)
+
+    visit new_news_path
+
+    fill_in :news_title, with: "Post title"
+    fill_in :news_body, with: "Some body content."
+    click_on "Create News"
+
+    expect(page).to have_css("h1", text: "Post title")
+  end
+end
