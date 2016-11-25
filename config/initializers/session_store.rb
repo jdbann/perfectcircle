eafa70e87a3efd3f8f542d565563c36a21cf56ec
John Bannister <jdbann@gmail.com>
2016-11-25 22:53:11 +0000

    Run update task

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Perfectcircle::Application.config.session_store :cookie_store,
-                                                key: "_perfectcircle_session"
+Rails.application.config.session_store :cookie_store,
+                                       key: "_perfectcircle_session"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 # http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
